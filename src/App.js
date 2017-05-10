076bdef28d1e93e993c5ed15be765580eefce668
PhilMayer <phillipmmayer@gmail.com>
2017-05-10 14:04:25 -0400

    Implement uploading to Box

--- src/App.js
+++ src/App.js
@@ -6,28 +6,41 @@ import $ from 'jquery';
 class App extends Component {
 
   sendFileToBox () {
-    const file = document.getElementById("myFile")
-    console.log(file.value)
+    // var form = new FormData();
+    //
+    // // The content of the file
+    // var file = '<p>hey!<p>';
+    //
+    // // JS file-like object
+    // var testing = new Blob([file], { type: 'text/xml'});
+    // // Add the file to the form
+    // form.append('file', testing);
+    //
+    // // Add the destination folder for the upload to the form
+    // form.append('parent_id', '0');
+    // debugger
+    //
+    // var uploadUrl = 'https://upload.box.com/api/2.0/files/content';
+    //
+    // // The Box OAuth 2 Header. Add your access token.
+    // var headers = {
+    //     Authorization: 'Bearer Is8CPcwVj8XWEVK4Mem4p4YUCtXcIcpZ'
+    // };
 
+    var selectedFile = document.getElementById('myFile').files[0];
     var form = new FormData();
-
-    // JS file-like object
-    var toUpload = new Blob([file.value], { type: 'pdf'});
-
-    // Add the file to the form
-    form.append('file', toUpload);
-
-    // Add the destination folder for the upload to the form
-    form.append('parent_id', '0');
-
+    form.append('file', selectedFile)
+    form.append('parent_id', '0')
+    // debugger
     $.ajax({
-      url: "https://upload.box.com/api/2.0/files/content",
-      headers: {Authorization: "Bearer VOe8RW3t78uwnsOIBjBX9nWySbDU3aFA"},
-      type: 'POST',
-      processData: false,
-      contentType: false,
-      data: form
-    }).then(() => console.log("success"))
+        url: 'https://upload.box.com/api/2.0/files/content',
+        headers: {Authorization: 'Bearer vkFk3gJwLmviyZDqe0p7Bcln7lKfLNHj'},
+        type: 'POST',
+        processData: false,
+        contentType: false,
+        dataType: 'json',
+        data: form
+    }).then(() => console.log("success!!!!"), (e) => console.log(e.responseText))
   }
 
   render() {
