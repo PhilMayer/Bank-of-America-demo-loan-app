7c09a129e792bf1a1ba84ac46f0496af3f46c277
PhilMayer <phillipmmayer@gmail.com>
2017-05-10 14:13:53 -0400

    Add favicon

--- src/App.js
+++ src/App.js
@@ -6,32 +6,11 @@ import $ from 'jquery';
 class App extends Component {
 
   sendFileToBox () {
-    // var form = new FormData();
-    //
-    // // The content of the file
-    // var file = '<p>hey!<p>';
-    //
-    // // JS file-like object
-    // var testing = new Blob([file], { type: 'text/xml'});
-    // // Add the file to the form
-    // form.append('file', testing);
-    //
-    // // Add the destination folder for the upload to the form
-    // form.append('parent_id', '0');
-    // debugger
-    //
-    // var uploadUrl = 'https://upload.box.com/api/2.0/files/content';
-    //
-    // // The Box OAuth 2 Header. Add your access token.
-    // var headers = {
-    //     Authorization: 'Bearer Is8CPcwVj8XWEVK4Mem4p4YUCtXcIcpZ'
-    // };
-
     var selectedFile = document.getElementById('myFile').files[0];
     var form = new FormData();
     form.append('file', selectedFile)
     form.append('parent_id', '0')
-    // debugger
+
     $.ajax({
         url: 'https://upload.box.com/api/2.0/files/content',
         headers: {Authorization: 'Bearer vkFk3gJwLmviyZDqe0p7Bcln7lKfLNHj'},
