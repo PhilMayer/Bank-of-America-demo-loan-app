3c5025020b278891eb94b403dcee06cc85df23ec
PhilMayer <phillipmmayer@gmail.com>
2017-05-10 23:54:24 -0400

    Display names of uploaded files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import $ from 'jquery';
 
 class App extends Component {
+  constructor () {
+    super();
+
+    this.state = {
+      fileUploaded: false,
+      files: []
+    }
+  }
 
   sendFileToBox () {
     var selectedFile = document.getElementById('myFile').files[0];
@@ -13,16 +21,25 @@ class App extends Component {
 
     $.ajax({
         url: 'https://upload.box.com/api/2.0/files/content',
-        headers: {Authorization: 'Bearer vkFk3gJwLmviyZDqe0p7Bcln7lKfLNHj'},
+        headers: {Authorization: 'Bearer 09tOHswNDmEMaIxK6c7epIZm2EXMwNyO'},
         type: 'POST',
         processData: false,
         contentType: false,
         dataType: 'json',
         data: form
-    }).then(() => console.log("success!!!!"), (e) => console.log(e.responseText))
+    }).then(() => this.setState({
+        fileUploaded: true,
+        files: this.state.files.concat([selectedFile])
+      })
+    )
   }
 
   render() {
+    let files;
+    if (this.state.files) {
+      files = this.state.files.map((file, idx) => <p key={idx}>{file.name}</p>)
+    }
+
     return (
       <div className="App">
         <div className="App-header">
@@ -31,6 +48,12 @@ class App extends Component {
 
         <div className="App-body">
           <h2>Upload your documents</h2>
+
+          <h3 className={this.state.fileUploaded ? "" : "hidden"}>
+            Upload successful. Your documents are being stored securely with Bank of America.
+          </h3>
+
+          <div>{files}</div>
 
           <input
             onChange={() => this.sendFileToBox()}
